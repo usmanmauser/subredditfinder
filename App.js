79d1ff71b61d3c4bfbe0c9b9a627dab1529691bb
usmanmauser <usmanibrahimmauser@gmail.com>
2025-08-17 14:50:13 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SubredditFinder from "./Pages/SubredditFinder";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Subreddit Finder</h1>
+      <SubredditFinder />
+    </div>
+  );
+}
