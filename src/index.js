71d3dedb6719096704c31ca6a070af734970161a
usmanmauser <usmanibrahimmauser@gmail.com>
2025-08-17 15:43:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";   // <-- important
-import App from "./App";
+import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
